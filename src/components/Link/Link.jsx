a0a018464e35ff92a43c5f2c66bb0f44d894f7ad
getahsaan <getahsaan@gamil.com>
2023-04-02 16:21:57 +0600

    added navbar

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = ({ route }) => {
     return (
         <div>
-            <li className='mr-12'>
+            <li className='mr-12 hover:bg-purple-600 p-1 md:px-2'>
                 <a href={route.path}>{route.name}</a>
             </li>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
     const routes = [
         { id: 1, name: 'Home', path: '/' },
         { id: 2, name: 'About', path: '/about' },
@@ -10,14 +12,16 @@ const Navbar = () => {
         { id: 4, name: 'Products', path: '/products' },
         { id: 5, name: 'Product Details', path: '/products/:id' },
     ];
-  
+
     return (
-        <div>
-            <ul className='md:flex'>
-                <Bars3Icon className="h-6 w-6 text-purple-500" />
+        <nav className='bg-purple-400'>
+            <div onClick={() => setOpen(!open)} className='cursor-pointer md:hidden'>
+                <span>{open ? <Bars3Icon className="h-6 w-6 text-purple-500" /> : <XMarkIcon className="h-6 w-6 text-purple-500" />}</span>
+            </div>
+            <ul className={`md:flex absolute md:static duration-500 bg-purple-400 p-8 ${open ? 'top-6' : '-top-96'}`}>
                 {routes.map(route => <Link key={route.id} route={route}></Link>)}
             </ul>
-        </div>
+        </nav >
     );
 };
 
